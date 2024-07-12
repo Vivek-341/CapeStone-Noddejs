d8b7975313a11022195d8278d28ffcbc10dea0c6
Vivek-341 <vivekvardhannada@gmail.com>
2024-07-12 21:24:48 +0530

    Update user-controller.js

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -37,9 +37,12 @@ export const loginUser = async (request, response) => {
         console.log('2');
         if (match) {
             console.log('3.1');
-            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
+            //const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
+            const accessToken = jwt.sign(user.toJSON(), 'de37bbab03b0380009e4b2f9c6fa6dcf95ae3acfc63125ce7b580d8d490eb6f92579843a4ce44011998dbec04fab568e5de224b042b2c2cf284acfbe03f1f4a6', { expiresIn: '15m'});
+            
             console.log('3.2');
-            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+            //const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+            const refreshToken = jwt.sign(user.toJSON(), 'a60327222ffaa884573490fc0574d69f3f5d1925a98479fb4ef184ebc99fab84cbb9edcc41434b58a07b66188a75152916ed7b46b2031afa5b5c0f046cacb726');
             console.log('3.3');
             const newToken = new Token({ token: refreshToken });
             console.log('4');
