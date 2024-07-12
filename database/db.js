7e617b60d87a1268975fa8017884f6aa25853831
Vivek-341 <vivekvardhannada@gmail.com>
2024-07-12 20:49:09 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const Connection = async (username, password) => {
     console.log(URL);
     try {
         console.log('MongoDB before');
-        await mongoose.connect(URL);
+        await mongoose.connect(URL, { useNewUrlParser: true })
         mongoose.set('strictQuery', false);
             //, { useNewUrlParser: true ,useUnifiedTopology: true,maxPoolSize: 10,socketTimeoutMS: 30000, keepAlive: true})
         console.log('MongoDB is connected');
